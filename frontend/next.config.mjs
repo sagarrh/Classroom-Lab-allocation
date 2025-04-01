a918d199b59cc5944e13851d6be033bf585e0184
sagarrh <harsorasagar04@gmail.com>
2025-04-01 21:37:12 +0530

    done

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,13 +1,11 @@
-import dotenv from 'dotenv';
-dotenv.config();
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  env: {
-    SUPABASE_URL: process.env.SUPABASE_URL,
-    SUPABASE_ANON_KEY: process.env.SUPABASE_ANON_KEY
-  }
-};
-
-export default nextConfig;
+    reactStrictMode: true,
+    env: {
+      SUPABASE_URL: process.env.SUPABASE_URL,
+      SUPABASE_ANON_KEY: process.env.SUPABASE_ANON_KEY
+    }
+  };
+  
+  export default nextConfig;
+  
